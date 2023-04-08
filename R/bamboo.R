4d33ccb0c575c23ae58a520e275b3ba90a6a7a32
gagolews <m.gagolewski@gmail.com>
2023-04-08 15:12:52 +1000

    S3 methods

--- R/bamboo.R
+++ R/bamboo.R
@@ -1,8 +1,14 @@
-prepare_message <- function(x) {
+prepare_message <- function(x)
     sprintf("G'day, %s!\n", x)
-}
 
 
-bamboo <- function(x="world") {
+bamboo <- function(x="world")
     cat(prepare_message(toTitleCase(x)))
-}
+
+
+print.koala <- function(x, ...)
+    cat(sprintf("This is a cute koala, %s\n", x))
+
+
+.a_hidden_method_to_print_a_roo <- function(x, ...)
+    cat(sprintf("This is a very naughty kangaroo, %s\n", x))
